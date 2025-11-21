1c45ce356b311c305cf9ed0a03fb5beef380e7af
ivanvs <luisvalenciasantes@gmail.com>
2025-11-21 11:36:54 -0600

    commit inicial del proyecto Django con hola IVS

---
+++ nucleo/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+
+def hola_mundo(request):
+    """
+    Una vista simple que retorna un saludo en HTML.
+    """
+    html_content = """
+    <div style='font-family: sans-serif; text-align: center; padding-top: 50px;'>
+        <h1>¡Hola IvanVS!</h1>
+        <p>Esta es mi primera app Django desplegada en PythonAnywhere con Github.</p>
+        <p>¡Funcionó!</p>
+    </div>
+    """
+    return HttpResponse(html_content)
